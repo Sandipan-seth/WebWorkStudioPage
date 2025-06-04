566b628a6498c89978e7ae27aac07b0d04a8638d
Sandipan <sandipanseth2004@gmail.com>
2025-06-04 20:12:01 +0530

    Starting from here

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import MainPage from "./pages/MainPage";
-import Meetup from "./components/Meetup";
-import ClientForm from "./components/ClientForm";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/meetup" element={<Meetup/>} />
-        <Route path="/clientform" element={<ClientForm />} />
+        <Route path="/" element={<Home />} />
       </Routes>
   );
 };
