8941c7ebe7da8e80cb01a913c3c3931796a8008d
Rupam-Mondal <rupammondal564@gmail.com>
2025-06-05 08:02:26 +0530

    Avatars added

--- src/components/HomeComponents/Animationtext/AnimationText.jsx
+++ src/components/HomeComponents/Animationtext/AnimationText.jsx
@@ -2,6 +2,7 @@ import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
 import { AnimatedShinyText } from "@/components/magicui/animated-shiny-text";
+import { AvatarCirclesDemo } from "../AvatarSection/AvatarCircles";
 
 export function AnimatedShinyTextDemo() {
   return (
@@ -12,7 +13,14 @@ export function AnimatedShinyTextDemo() {
         )}
       >
         <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 font-semibold text-black dark:text-white">
+          <span><AvatarCirclesDemo/></span>
+          <span
+            className="mx-1 inline-block h-6 border-l border-current align-middle"
+            aria-hidden="true"
+          />
+
           <span>Trusted by many clients ✨</span>
+
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </AnimatedShinyText>
       </div>
---
+++ src/components/HomeComponents/AvatarSection/AvatarCircles.jsx
@@ -0,0 +1,24 @@
+import { AvatarCircles } from "@/components/magicui/avatar-circles";
+
+const avatars = [
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/16860528",
+    profileUrl: "https://github.com/dillionverma",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/20110627",
+    profileUrl: "https://github.com/tomonarifeehan",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/106103625",
+    profileUrl: "https://github.com/BankkRoll",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+    profileUrl: "https://github.com/safethecode",
+  }
+];
+
+export function AvatarCirclesDemo() {
+  return <AvatarCircles avatarUrls={avatars} />;
+}
