c519b8626a6ca9ef7161f14398115eb59ea606d4
Rupam-Mondal <rupammondal564@gmail.com>
2025-06-07 07:14:21 +0530

    Updated seizes

--- src/components/HomeComponents/Marquee/Marquee.jsx
+++ src/components/HomeComponents/Marquee/Marquee.jsx
@@ -10,8 +10,10 @@ function BrandMarquee() {
   const repeatedImages = [...brandImages, ...brandImages, ...brandImages];
 
   return (
-    <div className="w-full py-10 px-20 mt-16 flex flex-col gap-16">
-      <h2 className="text-5xl md:text-7xl font-semibold text-center mb-8">Our Valued <span className="text-[#B9F33F] font-bold">Clients</span></h2>
+    <div className="w-full py-10 px-6 md:px-20 mt-12 flex flex-col gap-12">
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-center mb-8">
+        Our Valued <span className="text-[#B9F33F] font-bold">Clients</span>
+      </h2>
       <Marquee gradient={false} speed={60} pauseOnHover={true}>
         {repeatedImages.map((img, index) => (
           <div key={index} className="md:mx-12 mx-7 flex items-center">
